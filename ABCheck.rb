c0e6ebf471df632aa6ae635922513eca4032031f
newjeong <hwang920@naver.com>
2015-07-31 15:25:23 +0900

    Create ABCheck.rb

---
+++ ABCheck.rb
@@ -0,0 +1,19 @@
+def ABCheck(str)
+  
+  counter = 0
+  str.each_char do |x|
+    if(x == 'a' && counter + 4 < str.length && str.chars.to_a[counter + 4] == 'b')
+      return true
+    end
+    counter += 1
+  end
+  return false 
+         
+end
+
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ABCheck(STDIN.gets)  
+
+
